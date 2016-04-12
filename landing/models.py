dc70dd2304ad1e04da3e86a835d1e558ba4fb5a8
Johan de Maury <j.demaury@softnautic.com>
2016-04-12 12:44:45 +0200

    Landing: fix popacity, add requirements.txt

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models import EmailField
 
 # Create your models here.
+class ExternalMailingListSubscriber(models.Model):
+    email = EmailField(blank=False)
+
+    def postsave(self):
+    # send email after save
+        return
--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from .models import ExternalMailingListSubscriber
+
 # Create your views here.
 
 def comingsoon(request):
+    if request.POST:
+        if not ExternalMailingListSubscriber.objects.filter(email=request.POST['email']):
+            email_subscriber = ExternalMailingListSubscriber.objects.create( email=request.POST['email'] )
+            email_subscriber.save()
+
+# send email to confirm subscription
     return render(request,template_name='comingsoon.html',context=None)
 
 def welcome(request):
