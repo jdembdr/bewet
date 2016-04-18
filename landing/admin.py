e52e1fd2f8fc9c62537e49a23573d666c3bd8ad3
Johan de Maury <j.demaury@softnautic.com>
2016-04-18 14:34:45 +0200

    Fix css style, height of slideshow

--- landing/admin.py
+++ landing/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ExternalMailingListSubscriber
+
+@admin.register(ExternalMailingListSubscriber)
+class ExternalMailingListSubscriberAdmin(admin.ModelAdmin):
+    fields = ('email','subscription_date',)
+
--- landing/models.py
+++ landing/models.py
@@ -1,9 +1,14 @@
 from django.db import models
-from django.db.models import EmailField
+from django.db.models import EmailField, DateField
 
 # Create your models here.
 class ExternalMailingListSubscriber(models.Model):
     email = EmailField(blank=False)
+    subscription_date = DateField()
+
+
+    def __str__(self):
+        return self.email
 
     def postsave(self):
     # send email after save
